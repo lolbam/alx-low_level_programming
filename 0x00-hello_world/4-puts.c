2b8a42618d24ac4b12f06b6fcfd2431daa735430
Bamiwo <olalekanaluko123@gmail.com>
2022-06-03 09:51:05 -0700

    problem fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main (void)
+
+/**
+* main - This function prints out the string
+* "Programming is like building a multilingual puzzle
+* Return: Function returns 0
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
