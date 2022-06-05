6315b0c666910625e3224a76ae29425fc5add88b
Bamiwo <olalekanaluko123@gmail.com>
2022-06-05 14:35:03 -0700

    fixed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: returns 0 always
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		putchar(p % 10 + '0');
+		if (p > 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
