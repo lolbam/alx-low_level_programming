9776971a5ee991f6848dbad6db0fee0eddf87bc6
bamiwoaluko <105121241+bamiwoaluko@users.noreply.github.com>
2022-06-07 22:05:24 +0100

    checks alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  *_isalpha - checks for alphabetic character
- *@c - is the character
- * Return - Retruns 1 when it is a letter, lowercase or uppercase and returns 0 when otherwise
+ *@c: is the character
+ * Return: Retruns 1 when it is a letter, lowercase or uppercase and returns 0 when otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
