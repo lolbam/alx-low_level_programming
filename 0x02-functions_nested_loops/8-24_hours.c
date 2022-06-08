0c7ef509fdecc4085f123fcdbe4dc88e7bac01c7
Bamiwo <olalekanaluko123@gmail.com>
2022-06-08 05:28:14 -0700

    prints every minute of the day in 24 hour format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * starting from 00:00 to 23:59
+ * @p: is the minutes
+ * @r: is the hour
+ * Return: returns 0
+ */
+void jack_bauer(void)
+{
+	int p, r;
+
+	for (r = 0; r <= 23; r++)
+	{
+		for (p = 0; p <= 59; p++)
+		{
+			_putchar(r / 10 + '0');
+			_putchar(r % 10 + '0');
+			_putchar(':');
+			_putchar(p / 10 + '0');
+			_putchar(p % 10 + '0');
+			_putchar('\n');
+		}
+			
+	}
+}
