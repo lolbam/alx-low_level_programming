449bb8b95101a2d84b47e34bd1572c72ab4812a5
Bamiwo <olalekanaluko123@gmail.com>
2022-06-16 04:51:46 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/** rev_strings - reverses a string
+ * @s: string to be reversed
+ */
+
+
+void rev_string(char *s)
+{
+	int i, j, k, tempVal;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	k = 0;
+
+	while (k < j)
+	{
+		tempVal = s[k];
+		s[k] = s[j];
+		s[j] = tempVal;
+		k++;
+		j--;
+	}
+}
