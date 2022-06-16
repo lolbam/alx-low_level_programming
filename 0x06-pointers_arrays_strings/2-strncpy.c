a606597075cd7682e34ee482e7742c51688ef7d9
Bamiwo <olalekanaluko123@gmail.com>
2022-06-16 13:00:57 -0700

    done 2 and 104

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strncpy - Copies at most an inputted number
+*           of bytes from string src into dest
+* @dest: to be copied into
+* @src: to be copied to
+* @n: number of characters to copy
+*
+* Return: returns dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
