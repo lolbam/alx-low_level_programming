d1b128b7e870d5401b39d1f72f1f023d01c15bd2
Bamiwo <olalekanaluko123@gmail.com>
2022-06-20 14:49:42 -0700

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: 2 dimensional array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b;
+	int c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+			_putchar(a[b][c]);
+		_putchar('\n');
+	}
+}
